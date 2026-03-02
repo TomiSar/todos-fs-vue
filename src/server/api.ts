c77fe55dcd3be23cda5238aebbb9a1f697e758ee
TomiSar <tomppa.sarjamo@gmail.com>
2026-03-02 20:17:26 +0200

    Added saving tasks in databse functionality in application.

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,10 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { remultApi } from 'remult/remult-express';
 import { Task } from '../shared/task';
 import { TasksController } from '../shared/taskController';
+import { createKnexDataProvider } from 'remult/remult-knex';
 
 export const api = remultApi({
   entities: [Task],
   controllers: [TasksController],
   getUser: (req) => req.session!['user'],
   admin: true, // Enable the Admin UI
+  dataProvider: createKnexDataProvider({
+    client: 'mysql2',
+    connection: {
+      host: process.env['DB_HOST'],
+      database: process.env['DB_NAME'],
+      user: process.env['DB_USER'],
+      password: process.env['DB_PASSWORD'],
+    },
+  }),
 });
